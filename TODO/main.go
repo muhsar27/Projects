47e60122914ac503287d05172421f6c8920024e1
muhsar27 <muhsar4321@gmail.com>
2026-06-24 23:06:29 +0100

    feat: Initialised the (add) command

--- TODO/main.go
+++ TODO/main.go
@@ -6,24 +6,38 @@ import (
 )
 
 type Todo struct {
-	Task string
-	Done bool
+	Entry string `json:entry`
+	Done  bool   `json:done`
 }
 
+// commands for my todo list:
+// add(add a task), list(enumerate the tasks and status),
+// done(correct the status of one task to done), delete(delete a task)
 func main() {
-	args := os.Args
 
-	if len(args) < 2 {
-		fmt.Print("Usage: todo command [arguments]")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
 		return
 	}
+	var TaskList []Todo
+	switch args[0] {
+	case "add":
+		task := args[1]
+		var status bool
+		if args[2] == "Done" {
+			status = true
+		} else {
+			status = false
+		}
 
-	var taskList []Todo
-	command := args[1]
-	taskName := args[2]
+		newTodo := Todo{Entry: task, Done: status}
+		TaskList = append(TaskList, newTodo)
 
-	if command == "add" {
-		taskList.Task = taskName
+	default:
+		fmt.Print("Fuck off \n")
 	}
+
+	fmt.Println(TaskList)
 
 }
